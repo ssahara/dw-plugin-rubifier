dd896f87304f1e298f9a3998450cb96f2c86ba89
Satoshi Sahara <sahara.satoshi@gmail.com>
2018-07-14 10:25:29 +0900

    add notes of PCRE_UTF8 moddifier

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -4,4 +4,4 @@
  */
 
 $lang['parentheses'] = 'a pair of marks to enclose ruby text if browser/user agents does not support ruby annotations; ex. ()';
-$lang['rubify'] = 'Enable ruby annotations of Aozora-Bunko style markup';
+$lang['rubify'] = 'Enable ruby annotations of Aozora-Bunko style markup.<br>(require DW Lexer with u(PCRE_UTF8) modifier)';
--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -4,4 +4,4 @@
  */
 
 $lang['parentheses'] = 'ルビテキストを囲む括弧（rubyタグ非対応ブラウザ用）例：《》';
-$lang['rubify'] = '青空文庫形式のルビ記法を有効にする';
+$lang['rubify'] = '青空文庫形式のルビ記法を有効にする<br>(require DW Lexer with u(PCRE_UTF8) modifier)';
