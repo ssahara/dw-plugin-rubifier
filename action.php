82a2f71bb558872e860b78ea8caee8c47bcb5a16
Satoshi Sahara <sahara.satoshi@gmail.com>
2018-07-20 18:57:01 +0900

    update plugin.info

--- action.php
+++ action.php
@@ -20,7 +20,9 @@ class action_plugin_rubifier extends DokuWiki_Action_Plugin {
      */
     function register(Doku_Event_Handler $controller) {
         if (in_array('action', explode(',', $this->getConf('rubify')))) {
-            $controller->register_hook('RENDERER_CONTENT_POSTPROCESS', 'AFTER', $this, '_rubify');
+            $controller->register_hook(
+                'RENDERER_CONTENT_POSTPROCESS', 'AFTER', $this, '_rubify'
+            );
         }
     }
 
@@ -29,7 +31,7 @@ class action_plugin_rubifier extends DokuWiki_Action_Plugin {
      * Convert rubi-syntax to HTML5 ruby annotation
      * 青空文庫風のルビ記法を実現する。見出し文字列にも適用されます
      */
-    function _rubify(Doku_Event $event, $param) {
+    function _rubify(Doku_Event $event) {
 
         // load helper object
         isset($rubify) || $rubify = $this->loadHelper($this->getPluginName());
